44731af89367c42060a89c6a2eace6a71e53409d
Youssef Kadaoui Abbassi <youssef.kadaoui-abbassi@epita.fr>
2022-06-24 03:58:26 +0200

    Adding mantine

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,25 @@
 import React from 'react';
-import '../styles/globals.css';
-import type { AppProps } from 'next/app';
+import { AppProps } from 'next/app';
+import Head from 'next/head';
+import { MantineProvider } from '@mantine/core';
+import { theme } from '../src/constant/theme';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+export default function App(props: AppProps) {
+  const { Component, pageProps } = props;
+
+  return (
+    <>
+      <Head>
+        <title>Page title</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+
+      <MantineProvider withGlobalStyles withNormalizeCSS theme={theme}>
+        <Component {...pageProps} />
+      </MantineProvider>
+    </>
+  );
 }
-
-export default MyApp;
